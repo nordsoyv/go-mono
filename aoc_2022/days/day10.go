f6558e40e31e549e63876646c90dca37127bb27b
Øyvind Nordsveen <oyvind.nordsveen@varnertech.no>
2022-12-16 14:53:26 +0100

    Day 10 B done

--- aoc_2022/days/day10.go
+++ aoc_2022/days/day10.go
@@ -8,11 +8,12 @@ import (
 
 func Day10A() {
 	println("Day 10 A")
-	task10a("./res/day10.txt", -1)
+	task10a("./res/day10.txt", 13820)
 }
 
 func Day10B() {
 	println("Day 10 B")
+	task10b("./res/day10.txt", -1)
 }
 
 func Day10TestA() {
@@ -23,6 +24,7 @@ func Day10TestA() {
 
 func Day10TestB() {
 	println("Day 10 test B")
+	task10b("./res/day10test.txt", -1)
 }
 
 func task10a(path string, expected int) {
@@ -40,14 +42,13 @@ func task10a(path string, expected int) {
 		regX:         1,
 	}
 	machine.ExecuteProgram()
-	offset := -2
 	//fmt.Println(machine.log)
-	valueAt20 := machine.log[20+offset]
-	valueAt60 := machine.log[60+offset]
-	valueAt100 := machine.log[100+offset]
-	valueAt140 := machine.log[140+offset]
-	valueAt180 := machine.log[180+offset]
-	valueAt220 := machine.log[220+offset]
+	valueAt20 := machine.GetCycleValue(20)
+	valueAt60 := machine.GetCycleValue(60)
+	valueAt100 := machine.GetCycleValue(100)
+	valueAt140 := machine.GetCycleValue(140)
+	valueAt180 := machine.GetCycleValue(180)
+	valueAt220 := machine.GetCycleValue(220)
 	fmt.Println(valueAt20, 21)
 	fmt.Println(valueAt60, 19)
 	fmt.Println(valueAt100, 18)
@@ -61,6 +62,24 @@ func task10a(path string, expected int) {
 	result += 180 * valueAt180
 	result += 220 * valueAt220
 	fmt.Println("Signal strength sum is:", result, "Should be ", expected)
+}
+
+func task10b(path string, _ int) {
+	lines := common.ReadFileToLines(path)
+	instructions := make([]Instruction, 0)
+	for _, line := range lines {
+		instruction := parseInstruction(line)
+		instructions = append(instructions, instruction)
+	}
+	machine := Machine{
+		log:          make([]int, 0),
+		instructions: instructions,
+		cycle:        1,
+		regX:         1,
+	}
+	machine.ExecuteProgram()
+	machine.DrawCrt()
+
 }
 
 type Machine struct {
@@ -101,6 +120,40 @@ func (m *Machine) EndCycle() {
 	m.log = append(m.log, m.regX)
 	m.cycle++
 	fmt.Println(len(m.log), m.regX)
+}
+
+func (m Machine) GetCycleValue(cycle int) int {
+	offset := -1
+	if (cycle + offset) < 0 {
+		return 0
+	}
+	return m.log[cycle+offset]
+}
+
+func (m Machine) DrawCrt() {
+	crtLine := ""
+	for i := 0; i < len(m.log); i++ {
+		if i%40 == 0 {
+			fmt.Println(crtLine)
+			crtLine = ""
+		}
+		logValue := m.GetCycleValue(i)
+		if pixelHit(i%40, logValue) {
+			crtLine += "#"
+		} else {
+			crtLine += " "
+		}
+
+	}
+	fmt.Println(crtLine)
+
+}
+
+func pixelHit(cycle, pos int) bool {
+	if cycle == pos || cycle == pos+1 || cycle == pos-1 {
+		return true
+	}
+	return false
 }
 
 func parseInstruction(input string) Instruction {
