aa793ad14f6cae39d3382739b92910fa7905c8d7
Øyvind Nordsveen <oyvind.nordsveen@varnertech.no>
2022-12-21 09:36:20 +0100

    Update template

--- aoc_2022/days/day_template.go
+++ aoc_2022/days/day_template.go
@@ -2,16 +2,27 @@ package days
 
 func DayXXA() {
 	println("Day XX A")
+	taskXXa("", -1)
 }
 
 func DayXXB() {
 	println("Day XX B")
+	taskXXb("", -1)
 }
 
 func DayXXTestA() {
 	println("Day XX test A")
+	taskXXa("", -1)
 }
 
 func DayXXTestB() {
 	println("Day XX test B")
+	taskXXb("", -1)
+}
+
+func taskXXa(path string, expected int) {
+
+}
+func taskXXb(path string, expected int) {
+
 }
