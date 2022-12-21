98a09fbc72c0a3e8bf5a29151890b55b5856996a
Øyvind Nordsveen <oyvind.nordsveen@varnertech.no>
2022-12-21 10:17:50 +0100

    Day 12 support functions

--- aoc_2022/days/day12.go
+++ aoc_2022/days/day12.go
@@ -1,8 +1,13 @@
 package days
 
+import (
+	"aoc_2022/common"
+	"fmt"
+)
+
 func Day12A() {
 	println("Day 12 A")
-	task12a("", -1)
+	task12a("./res/day12.txt", -1)
 }
 
 func Day12B() {
@@ -12,7 +17,7 @@ func Day12B() {
 
 func Day12TestA() {
 	println("Day 12 test A")
-	task12a("", -1)
+	task12a("./res/day12test.txt", -1)
 }
 
 func Day12TestB() {
@@ -21,8 +26,106 @@ func Day12TestB() {
 }
 
 func task12a(path string, expected int) {
-
+	lines := common.ReadFileToLines(path)
+	m := makeMap()
+	m.readMap(lines)
 }
 func task12b(path string, expected int) {
 
+}
+
+type Map struct {
+	startX, startY int
+	endX, endY     int
+	data           [][]int32
+}
+
+func makeMap() Map {
+	m := Map{
+		startX: -1,
+		startY: -1,
+		endX:   -1,
+		endY:   -1,
+		data:   make([][]int32, 0),
+	}
+	return m
+}
+
+func (m *Map) readMap(input []string) {
+	for y, line := range input {
+		row := make([]int32, 0)
+		for x, tile := range line {
+			if tile == 83 {
+				row = append(row, 0)
+				m.startX = x
+				m.startY = y
+				continue
+			}
+			if tile == 69 {
+				row = append(row, 123-97)
+				m.endX = x
+				m.endY = y
+				continue
+			}
+			row = append(row, tile-97)
+		}
+		m.data = append(m.data, row)
+	}
+}
+
+func (m Map) String() string {
+	res := fmt.Sprintf("Start is (%3d,%3d)\n", m.startX, m.startY)
+	res += fmt.Sprintf("End is   (%3d,%3d)\n", m.endX, m.endY)
+
+	for _, row := range m.data {
+		for _, tile := range row {
+			res += fmt.Sprintf("%2d ", tile)
+		}
+		res += "\n"
+	}
+	return res
+}
+
+func (m Map) getTileHeight(x, y int) int32 {
+	if y < 0 || y >= len(m.data) {
+		return 1000
+	}
+	if x < 0 || x >= len(m.data[y]) {
+		return 1000
+	}
+	return m.data[y][x]
+}
+
+type coord struct {
+	x, y int
+}
+
+func (m Map) getLegalMoves(x, y int) []coord {
+	coords := make([]coord, 0)
+	currentHeight := m.getTileHeight(x, y)
+	if m.getTileHeight(x+1, y) == currentHeight+1 || m.getTileHeight(x+1, y) <= currentHeight {
+		coords = append(coords, coord{
+			x: x + 1,
+			y: y,
+		})
+	}
+	if m.getTileHeight(x-1, y) == currentHeight+1 || m.getTileHeight(x-1, y) <= currentHeight {
+		coords = append(coords, coord{
+			x: x - 1,
+			y: y,
+		})
+	}
+	if m.getTileHeight(x, y+1) == currentHeight+1 || m.getTileHeight(x, y+1) <= currentHeight {
+		coords = append(coords, coord{
+			x: x,
+			y: y + 1,
+		})
+	}
+	if m.getTileHeight(x, y-1) == currentHeight+1 || m.getTileHeight(x, y-1) <= currentHeight {
+		coords = append(coords, coord{
+			x: x,
+			y: y - 1,
+		})
+	}
+	return coords
 }
