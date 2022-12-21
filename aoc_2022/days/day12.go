869cbbfd76ee5f11864256d4f9bf27f5cf17d7e6
Øyvind Nordsveen <oyvind.nordsveen@varnertech.no>
2022-12-21 13:58:34 +0100

    Day 12 WIP

--- aoc_2022/days/day12.go
+++ aoc_2022/days/day12.go
@@ -105,32 +105,144 @@ type coord struct {
 	x, y int
 }
 
-func (m Map) getLegalMoves(x, y int) []coord {
+type pathFragment struct {
+	location coord
+	cost     int
+	fCost    int
+	parent   *pathFragment
+}
+
+func (m Map) getLegalMoves(pos coord) []coord {
 	coords := make([]coord, 0)
-	currentHeight := m.getTileHeight(x, y)
-	if m.getTileHeight(x+1, y) == currentHeight+1 || m.getTileHeight(x+1, y) <= currentHeight {
+	currentHeight := m.getTileHeight(pos.x, pos.y)
+	if m.getTileHeight(pos.x+1, pos.y) == currentHeight+1 || m.getTileHeight(pos.x+1, pos.y) <= currentHeight {
 		coords = append(coords, coord{
-			x: x + 1,
-			y: y,
+			x: pos.x + 1,
+			y: pos.y,
 		})
 	}
-	if m.getTileHeight(x-1, y) == currentHeight+1 || m.getTileHeight(x-1, y) <= currentHeight {
+	if m.getTileHeight(pos.x-1, pos.y) == currentHeight+1 || m.getTileHeight(pos.x-1, pos.y) <= currentHeight {
 		coords = append(coords, coord{
-			x: x - 1,
-			y: y,
+			x: pos.x - 1,
+			y: pos.y,
 		})
 	}
-	if m.getTileHeight(x, y+1) == currentHeight+1 || m.getTileHeight(x, y+1) <= currentHeight {
+	if m.getTileHeight(pos.x, pos.y+1) == currentHeight+1 || m.getTileHeight(pos.x, pos.y+1) <= currentHeight {
 		coords = append(coords, coord{
-			x: x,
-			y: y + 1,
+			x: pos.x,
+			y: pos.y + 1,
 		})
 	}
-	if m.getTileHeight(x, y-1) == currentHeight+1 || m.getTileHeight(x, y-1) <= currentHeight {
+	if m.getTileHeight(pos.x, pos.y-1) == currentHeight+1 || m.getTileHeight(pos.x, pos.y-1) <= currentHeight {
 		coords = append(coords, coord{
-			x: x,
-			y: y - 1,
+			x: pos.x,
+			y: pos.y - 1,
 		})
 	}
 	return coords
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func (m Map) costToGoal(tile coord) int {
+	return Abs(tile.x-m.end.x) + Abs(tile.y-m.end.y)
+}
+
+func findLowestCostFragmentPosition(list []pathFragment) int {
+	lowest := 100000
+	foundPos := 0
+	for pos, fragment := range list {
+		//fragment := fragment
+		if fragment.fCost < lowest {
+			lowest = fragment.fCost
+			foundPos = pos
+		}
+	}
+	return foundPos
+}
+
+func remove(s []pathFragment, i int) []pathFragment {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func (m Map) aStar() {
+
+	// A* Search Algorithm
+	//1.  Initialize the open list
+	open := make([]pathFragment, 0)
+	//2.  Initialize the closed list
+	closed := make([]pathFragment, 0)
+	//    put the starting node on the open
+	//    list (you can leave its f at zero)
+	//
+	start := pathFragment{
+		location: m.start,
+		cost:     0,
+		parent:   nil,
+		fCost:    m.costToGoal(m.start),
+	}
+	open = append(open, start)
+	//3.  while the open list is not empty
+	for true {
+		//    a) find the node with the least f on
+		//       the open list, call it "q"
+		qPos := findLowestCostFragmentPosition(open)
+		//    b) pop q off the open list
+		q := open[qPos]
+		open = remove(open, qPos)
+		//    c) generate q's 4 successors and set their
+		//       parents to q
+		//    d) for each successor
+		legalMoves := m.getLegalMoves(q.location)
+		for _, move := range legalMoves {
+			if move == m.end {
+				//        i) if successor is the goal, stop search
+
+			}
+			//        ii) else, compute both g and h for successor
+			//          successor.g = q.g + distance between
+			//                              successor and q
+			//          successor.h = distance from goal to
+			//          successor (This can be done using many
+			//          ways, we will discuss three heuristics-
+			//          Manhattan, Diagonal and Euclidean
+			//          Heuristics)
+			//
+			//          successor.f = successor.g + successor.h
+
+			nextFragment := pathFragment{
+				location: move,
+				cost:     q.cost + 1,
+				fCost:    q.cost + 1 + m.costToGoal(move),
+				parent:   &q,
+			}
+
+		}
+
+	}
+	//
+	//
+	//
+
+	//
+	//
+	//        iii) if a node with the same position as
+	//            successor is in the OPEN list which has a
+	//           lower f than successor, skip this successor
+	//
+	//        iV) if a node with the same position as
+	//            successor  is in the CLOSED list which has
+	//            a lower f than successor, skip this successor
+	//            otherwise, add  the node to the open list
+	//     end (for loop)
+	//
+	//    e) push q on the closed list
+	//    end (while loop)
+
 }
